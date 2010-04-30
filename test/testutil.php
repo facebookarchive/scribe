86454eaf46b1dd90c17779529d58261be75d75e7
groys <groys@facebook.com>
2010-04-30 11:29:13 -0700

    fixes to return value in testutil

--- test/testutil.php
+++ test/testutil.php
@@ -69,8 +69,8 @@ function scribe_stop($scribe_ctrl_path, $port, $pid = 0) {
   system($command, $error);
   echo "$command => $error\n";
 
-  if ($error === false) {
-    return false;
+  if ($error == 0) {
+    return true;
   }
 
   sleep($wait);
@@ -80,7 +80,8 @@ function scribe_stop($scribe_ctrl_path, $port, $pid = 0) {
     system('kill -9 ' . $pid, $success);
     echo "kill -9 $pid => $success\n";
 
-    if ($success === false) {
+    // If the kill failed
+    if ($success == -1 ) {
       print("ERROR: scribed did not stop after $wait seconds.\n");
       return false;
     }
